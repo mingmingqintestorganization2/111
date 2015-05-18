97c7e91848c4b44eb001a475072452a926c3d7ad
“mark” <mingmingqin@126.com>
2015-05-18 09:58:42 +0800

    Initial Commit

---
+++ Test222/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test222
+//
+//  Created by MARK_QIN on 15/5/18.
+//  Copyright (c) 2015年 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test222/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test222
+//
+//  Created by MARK_QIN on 15/5/18.
+//  Copyright (c) 2015年 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
